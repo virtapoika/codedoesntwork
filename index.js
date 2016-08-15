f89b29f6d236db693eba0c75d32796d6c3a82e32
Max <kalhama.max@gmail.com>
2016-08-15 08:26:56 +0300

    index.js is broken

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+var used = {};
+
 function randomize()
 {
-  var data = JSON.parse(data);
-  var random = Math.floor((Math.random() * data.length) + 0)
+  $.getJSON("data.json", function(data) {
+    var random = getNewRandom(data);
+    $('#tododiv').empty().append(data[random]);
+  });
 
-  
+  function getNewRandom(data) {
+    if(used.length >= data.length) {
+      return -1;
+    } else {
+      while (true) {
+        var random = Math.floor((Math.random() * data.length) + 0) //get random value
+        if ($.inArray(random, used) == -1) {//check if value is new
+          used.push(random); //add value to used
+          return random; //return value
+        }
+      }
+    }
+  }
 }
